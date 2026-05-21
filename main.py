46f832967f7a79b2015eadac33edb8ccf820fe68
thanusrisakthi14-oss <thanusrisakthi14@gmail.com>
2026-05-21 21:16:00 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import cv2
+cap=cv2.videocapture(0)
+
+while True:
+    ret,frame=cv2.read()
+    if not ret:
+        print("camera failed to capture")
+        break
+    resized=cv2.resize(frame,(640,480))
+    gray=cv2.cvtColor(resized,cv2.COLOR_BGR2GRAY)
+    cv2.imshow("Original",resized)
+    cv2.imshow("Grayscale",gray)
+    key=cv2.waitKey(1)
+    if key == ord("s"):
+        cv2.imwrite("Captured_image.jpg",gray)
+        print("Image Saved")
+    if key==ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
